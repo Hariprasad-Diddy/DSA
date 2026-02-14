f35c32fc66d10e5aff8902dc796a3d2463f5628e
HariPrasad <58254570+Hariprasad-Diddy@users.noreply.github.com>
2026-02-14 12:36:06 +0530

    fixed the issue for negative exponent

--- 01-Math/Power/power_of_a_number.py
+++ 01-Math/Power/power_of_a_number.py
@@ -1,17 +1,22 @@
-def _power_of_a_number(base : int, exponent_number : int) -> float:
-  """
-  This function calculates the power of a number
-  Args:
-      base number (int): The base number
-      exponent_number (int): The exponent number
-  Returns:
-      float: The power of a number
-  """
+def _power_of_a_number(base_number : int,exponent_number : int) -> float:
+    """
+    This function calculates the power of a number
+    Args:
+        base_number (int): The base number
+        exponent_number (int): The exponent number
+    Returns:
+        int: The power of the number
+    """    
+    power_of_a_number : float = 1
+    if exponent_number == 0: return 1
+    if base_number == 0 and exponent_number < 0: raise ValueError("ZeroDivisionError or ValueError")
+    
+    for _ in range(abs(exponent_number)):
+        power_of_a_number *= base_number
 
-  power_of_a_number : float = 1.0
-  for _ in range(exponent_number):
-    power_of_a_number *= base_number
-  return power_of_a_number
+    if exponent_number < 0: return 1 / power_of_a_number
+    
+    return power_of_a_number
 
 
 if __name__ == "__main__":
